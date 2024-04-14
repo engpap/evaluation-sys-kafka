ae795f523fc72c40db43913c9c7bcb70b46a21f1
engpap <10701904@polimi.it>
2024-04-14 18:09:20 -0500

    modified port to be exposed in dockerfiles

--- cli-app/cmd/internal/config/config.go
+++ cli-app/cmd/internal/config/config.go
@@ -1,7 +1,7 @@
 package config
 
 const (
-	UserServiceURL    = "http://localhost:8083"
-	ProjectServiceURL = "http://localhost:8084"
-	CourseServiceURL  = "http://localhost:8080"
+	UserServiceURL    = "http://Eval-sys-user-env-docker.eba-qj3fh5wc.eu-north-1.elasticbeanstalk.com"
+	ProjectServiceURL = "http://Eval-sys-project-env-docker.eba-tx9pz6g2.eu-north-1.elasticbeanstalk.com"
+	CourseServiceURL  = "http://Eval-sys-course-env-docker.eba-ij33i5hc.eu-north-1.elasticbeanstalk.com"
 )
